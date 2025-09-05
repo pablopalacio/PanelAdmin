ddc5eb6c0a1d12c2188a4a95e658aa055bd31cfc
Pablop <palaciopablo92@gmail.com>
2025-09-05 18:04:14 -0300

    Cree el ControlPerfil, modifique el app.jsx, modifique tablas y reutilice la useApi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,6 +6,7 @@ import Controllers from "./pages/Controllers";
 import Reclutadores from "./pages/Reclutador";
 import HoraServicio from "./pages/HoraServicio";
 import Escuela from "./pages/Escuela";
+import ControlPerfil from "./pages/ControlPerfil"; 
 import PrivateRoute from "./components/PrivateRoute";
 import "./index.css";
 
@@ -15,6 +15,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
+          {/* Rutas públicas */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
 
@@ -56,6 +57,16 @@ function App() {
             element={
               <PrivateRoute>
                 <Escuela />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Ruta dinámica de perfil */}
+          <Route
+            path="/control-perfil/:id"
+            element={
+              <PrivateRoute>
+                <ControlPerfil />
               </PrivateRoute>
             }
           />
