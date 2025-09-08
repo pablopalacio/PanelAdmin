b98624f6ab910bac105ae03f94c42a3555383ebf
JeffGT <jeff.giltoribio@gmail.com>
2025-09-08 10:27:28 -0500

    estilando home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,26 +8,30 @@ function Home() {
   };
 
   return (
-    <div>
-      <header
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "1rem",
-          background: "#f4f4f4",
-        }}
-      >
-        <h2>Bienvenido a Funval</h2>
-        <button
-          onClick={handleLoginClick}
-          className=" py-3 px-4 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700 transition-colors duration-200 shadow-md cursor-pointer"
-        >
-          Acceder
-        </button>
-      </header>
-      <main style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Página principal</h1>
-      </main>
+    <div
+      className="min-h-screen bg-cover bg-center text-white font-sans"
+      style={{
+        backgroundImage:
+          "url('https://www.estudiantefunval.org/pluginfile.php/1/theme_moove/sliderimage1/1755880041/Earth%20day.gif')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="min-h-screen bg-black/50 flex flex-col">
+        <header className="flex justify-between items-center px-6 py-4">
+          <img
+            src="https://cdn.brandfetch.io/idYqRqXUhP/w/6667/h/1667/theme/light/logo.png?c=1dxbfHSJFAPEGdCLU4o5B"
+            alt=""
+            className="w-35"
+          />
+          <button
+            onClick={handleLoginClick}
+            className="py-2 px-5 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white rounded-lg font-semibold transition duration-200 shadow-md"
+          >
+            Acceder
+          </button>
+        </header>
+      </div>
     </div>
   );
 }
