631e8f8661f949b58bbb62603c6d8c47ece95660
gnehemias04 <gnehemias04@gmail.com>
2025-09-10 17:53:26 -0300

    arreglos de sintaxis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,6 @@ function Home() {
   const handleLoginClick = () => {
     navigate("/login");
   };
-  const handlecontroller = () => {
-    navigate("/controller");
-  };
-  const handleestudiante = () => {
-    navigate("/panelestudiante");
-  };
-
-  const handlereclutador = () => {
-    navigate("/reclutador");
-  };
 
   return (
     <div
@@ -39,24 +29,6 @@ function Home() {
             className="py-2 px-5 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white rounded-lg font-semibold transition duration-200 shadow-md"
           >
             Acceder
-          </button>
-          <button
-            onClick={handlecontroller}
-            className="py-2 px-5 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white rounded-lg font-semibold transition duration-200 shadow-md"
-          >
-            controller
-          </button>
-          <button
-            onClick={handleestudiante}
-            className="py-2 px-5 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white rounded-lg font-semibold transition duration-200 shadow-md"
-          >
-            panelEstudiante
-          </button>
-          <button
-            onClick={handlereclutador}
-            className="py-2 px-5 bg-blue-600 hover:bg-blue-700 cursor-pointer text-white rounded-lg font-semibold transition duration-200 shadow-md"
-          >
-            reclutadores
           </button>
         </header>
       </div>
